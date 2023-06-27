f6f5389961dcc983f336bc7d79a7f4c809f4a8ad
StefanyMR <stefanymr208@gmail.com>
2023-06-27 11:15:34 -0300

    mudançinhas

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users cool. */
+router.get('/cool', function(req, res, next) {
+ // res.render('index', { title: 'you are so cool' });
+ res.send('you are so cool');
+});
 module.exports = router;
