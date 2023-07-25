218d43f68a73f9117f7a8c45fbeb51ef2551f981
StefanyMR <stefanymr208@gmail.com>
2023-07-25 10:44:18 -0300

    rotas/diretorios atividade

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,17 @@ router.get('/cool', function(req, res, next) {
  // res.render('index', { title: 'you are so cool' });
  res.send('you are so cool');
 });
+
+/* GET users cool. */
+router.get('/cool/beach', function(req, res, next) {
+  // res.render('index', { title: 'prainha!' });
+  res.send('Adoro Praia!');
+ });
+
+ /* GET users cool. */
+router.get('/ifc', function(req, res, next) {
+  // res.render('index', { title: 'Bem vindo!' });
+  res.send('Bem vindo ao IFC!!');
+ });
+
 module.exports = router;
